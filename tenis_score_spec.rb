1d68ad83eeb11fb97763a06eecdd29a93d232363
trykystm <trykystm@gmail.com>
2018-11-28 17:09:23 +0900

    Update tenis_score_spec.rb

--- tenis_score_spec.rb
+++ tenis_score_spec.rb
@@ -1,5 +1,14 @@
 require './tenis_score'
-RSpec.dedcribe TenisScore do
+RSpec.describe TenisScore do
   desctibe '#list' do
+    subject do
+      tenis_score = TenisScore.new
+      tenis_score.configuration(conf)
+      tenis_score.winA
+      tenis_score.list
+    end
+    context 'when sideA win' do
+      it{is_expected eq [[0, 0, 1], [0, 0, 0]]}
+    end
   end
 end
